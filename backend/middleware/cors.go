50c20072b2c6c5dbfbc4b61639cf6bab4c5ca224
hayzamjs <hayzam@gmail.com>
2024-06-29 17:16:57 +0530

    middleware: cors: go to next

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -9,6 +9,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers:", "Origin, Content-Type, X-Auth-Token, Authorization")
-		w.Header().Set("Content-Type", "application/json")
+
+		next.ServeHTTP(w, r)
 	})
 }
