2956d6d011ddb4c8a735720db1cff865893244af
ajasma6570 <ajasmaprince@gmail.com>
2024-08-04 02:46:48 +0530

    create documentation placeholder updated

--- frontend/src/components/CreateDocumentModal/CreateDocModal.jsx
+++ frontend/src/components/CreateDocumentModal/CreateDocModal.jsx
@@ -441,7 +441,7 @@ export default function CreateDocModal () {
                       value={formData?.baseURL}
                       type="text"
                       name="baseURL"
-                      placeholder={t('paste_your_url')}
+                      placeholder={t('paste_your_base_url')}
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     />
                   </div>
