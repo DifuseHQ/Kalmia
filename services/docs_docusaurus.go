779fab30c161a85932e718ebb2df5ddacee16828
hayzam <hayzam@gmail.com>
2024-08-08 23:15:34 +0530

    docs: keep dummy homepage even if lander

--- services/docs_docusaurus.go
+++ services/docs_docusaurus.go
@@ -1043,43 +1043,7 @@ func (service *DocService) WriteHomePage(documentation models.Documentation) err
 	var homePage string
 
 	if documentation.LanderDetails != "" && documentation.LanderDetails != "{}" {
-		homePage = fmt.Sprintf(`---
-		pageType: home
-		
-		hero:
-		  name: Kalmia
-		  text: Fast, easy-to-use CMS for your documentations
-		  tagline: Tagline
-		  actions:
-			- theme: brand
-			  text: Quick Start
-			  link: /guide/
-			- theme: alt
-			  text: GitHub
-			  link: https://github.com/web-infra-dev/rspress
-		  image:
-			src: https://difuse.io/assets/images/meta/meta.webp
-			alt: Rspress Logo
-		features:
-		  - title: Blazing fast build speed
-			details: The core compilation module is based on the Rust front-end toolchain, providing a more ultimate development experience.
-			icon: 🏃🏻‍♀️
-		  - title: Support for MDX content writing
-			details: MDX is a powerful way to write content, allowing you to use React components in Markdown.
-			icon: 📦
-		  - title: Built-in full-text search
-			details: Automatically generates a full-text search index for you during construction, providing out-of-the-box full-text search capabilities.
-			icon: 🎨
-		  - title: Simpler I18n solution
-			details: With the built-in I18n solution, you can easily provide multi-language support for documents or components.
-			icon: 🌍
-		  - title: Static site generation
-			details: In production, it automatically builds into static HTML files, which can be easily deployed anywhere.
-			icon: 🌈
-		  - title: Providing multiple custom capabilities
-			details: Through its extension mechanism, you can easily extend theme UI and build process.
-			icon: 🔥
-		---`)
+		homePage = fmt.Sprintf("<meta http-equiv=\"refresh\" content=\"0;url=%s\" />", documentation.BaseURL)
 	} else {
 		homePage = fmt.Sprintf("<meta http-equiv=\"refresh\" content=\"0;url=%s\" />", documentation.BaseURL)
 	}
