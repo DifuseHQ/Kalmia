5c50f4c09cec257d564291f0eb29e922b1d8a71e
ajasma <ajasma@difuse.io>
2024-08-14 09:51:43 +0530

    EditDoc: emoji pick value prop error fixed

--- web/src/components/CreateDocumentModal/CreateDocModal.jsx
+++ web/src/components/CreateDocumentModal/CreateDocModal.jsx
@@ -873,9 +873,10 @@ export default function CreateDocModal () {
                         <input
                           ref={(el) => (inputRefs.current[index] = el)}
                           onFocus={() => toggleEmojiPicker(index)}
-                          placeholder={convertToEmoji('26a1')}
+                          placeholder={`${convertToEmoji('26a1')} ${t('pick_your_emoji')}`}
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                           value={convertToEmoji(obj.emoji)}
+                          readOnly
                         />
                         {activeFieldIndex === index && showEmojiPicker && (
                           <div
