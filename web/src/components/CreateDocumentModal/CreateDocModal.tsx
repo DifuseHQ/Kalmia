b53b5b8d5fd32557c3d9d57aaec7fd1ceea429a8
ajasma <ajasma@difuse.io>
2024-09-03 14:25:34 +0530

    createDoc: authentication toggle added

--- web/src/components/CreateDocumentModal/CreateDocModal.tsx
+++ web/src/components/CreateDocumentModal/CreateDocModal.tsx
@@ -220,6 +220,7 @@ export default function CreateDocModal() {
   const { openModal, closeModal, setLoadingMessage } = useContext(ModalContext);
   const themeContext = useContext(ThemeContext);
   const { darkMode } = themeContext as ThemeContextType;
+  const [isAuthenticationToggleOn, SetIsAuthenticationToggleOn] = useState<boolean | undefined>(false);
   const [isToggleOn, SetIsToggleOn] = useState<boolean>(false);
   const [activeFieldIndex, setActiveFieldIndex] = useState<number | null>(null);
   const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
@@ -302,7 +303,7 @@ export default function CreateDocModal() {
               : JSON.parse(data.footerLabelLinks || "[]");
 
             setSocialPlatformField(footerLabelLinks);
-
+            SetIsAuthenticationToggleOn(data.requireAuth);
             const moreLabelLinks: MoreLabelLinks[] = Array.isArray(
               data?.moreLabelLinks,
             )
@@ -362,6 +363,7 @@ export default function CreateDocModal() {
         copyrightText: "",
         metaImage: "",
       });
+      SetIsAuthenticationToggleOn(false);
       setSocialPlatformField([{ icon: "", link: "" }]);
       setMoreField([{ label: "", link: "" }]);
       setLandingPage({
@@ -471,6 +473,7 @@ export default function CreateDocModal() {
       navImage: formData.navImage || "",
       copyrightText: formData.copyrightText || "",
       metaImage: formData.metaImage || "",
+      requireAuth:isAuthenticationToggleOn || false,
       landerDetails: JSON.stringify(landingData),
       footerLabelLinks: socialPlatformField
         ? JSON.stringify(socialPlatformField)
@@ -585,6 +588,7 @@ export default function CreateDocModal() {
     updateFeature(index, "emoji", emojiObject.unified);
     setShowEmojiPicker(false);
   };
+console.log(isAuthenticationToggleOn);
 
   return (
     <AnimatePresence>
@@ -604,10 +608,27 @@ export default function CreateDocModal() {
                 : t("new_documentation")}
             </h3>
           </div>
+          
 
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-800 sm:p-3">
             <div className="overflow-auto p-1">
               <div className="space-y-6">
+                <div  className="grid gap-4 sm:grid-cols-1">
+                <label className="inline-flex items-center cursor-pointer gpa-5 mb-4">
+                <span className="text-lg font-medium text-gray-900 dark:text-gray-300 mr-3">
+                  {t("enableAuthentication")}
+                </span>
+                <input
+                  type="checkbox"
+                  checked={isAuthenticationToggleOn}
+                  onChange={(e) => {
+                    SetIsAuthenticationToggleOn(e.target.checked);
+                  }}
+                  className="sr-only peer"
+                />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              </label>
+                </div>
                 <div className="grid gap-4 sm:grid-cols-2">
                   <FormField
                     label={t("title_label")}
